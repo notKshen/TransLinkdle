561aa4387c93d9bd350e125519f4b405665d3a9b
notKshen <shenkobe.111@gmail.com>
2025-08-26 19:23:27 -0700

    Initial Commit

---
+++ TransLinkdle/TransLinkdleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TransLinkdleApp.swift
+//  TransLinkdle
+//
+//  Created by Kobe Shen on 2025-08-26.
+//
+
+import SwiftUI
+
+@main
+struct TransLinkdleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
